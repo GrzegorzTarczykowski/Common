1860d3fcee5c2fab11e949d821bda5c2708c333d
Grzegorz Tarczykowski <grzegorz.tarczykowski@gmail.com>
2023-12-29 13:13:01 +0100

    grant permision user to service

---
+++ Common/PowerShell7/Script.ps1
@@ -0,0 +1,5 @@
+﻿$userSid = (gwmi win32_userprofile | select localpath, sid | where-object {$_.localpath -Match "Grzegorz"} | select-object sid).sid
+$currentServiceSettings = sc.exe sdshow "Usługa"
+$userSidPermisionToReplace = "(A;;RPWPCR;;;" + $userSid + ")S:"
+$newServiceSettings = $currentServiceSettings - replace "S:", $userSidPermisionToReplace
+sc.exe sdset "Usługa" $newServiceSettings | out-null
